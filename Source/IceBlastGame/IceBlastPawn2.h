6e032f7d3f3fa012a7a70b4688c421058bb567de
Savannahw22 <savannahwesley@outlook.com>
2022-03-20 10:37:51 +0000

    commit

---
+++ Source/IceBlastGame/IceBlastPawn2.h
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "IceBlastPawn2.generated.h"
+
+UCLASS()
+class ICEBLASTGAME_API AIceBlastPawn2 : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AIceBlastPawn2();
+	
+		// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// UPROPERTYS
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setup")
+	USkeletalMeshComponent* Ice;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setup")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setup")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Floats", meta = (ToolTip = "How fast the player goes, dependent of friction"))
+	float SideForce;
+
+	UPROPERTY(EditAnywhere, Category = "Floats", meta = (ToolTip = "The Higher The Value, The More Slide (Between 0-1)"))
+	float ReducedSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Floats")
+	float JumpHeight;
+
+	
+	class AIceBlastGameGameMode* GameMode;
+
+	
+	bool bGameEnded;
+	float Mass;
+	float DeltaSeconds;
+	float CurrentAxisValue;
+	bool bJump;
+	float DefaultSideForce;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent * MyComp, AActor * Other, UPrimitiveComponent * OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void OnLanded();
+	//Small Change
+	
+
+    
+	//Functions
+	void GameEnded();
+	void MoveRight(float AxisValue);
+	void EndGame();
+	void Jump();
+	void StopJump();
+	void SetSideForce();
+
+private:
+
+
+};
